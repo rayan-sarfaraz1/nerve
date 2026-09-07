0c7f0c62a8c2016c7fd39ca1d513d5ea7a44672d
Rayan Sarfaraz <rayan.sarfaraz1@example.com>
2026-09-08 03:04:50 +0500

    Add AdMob rewarded video revive popup when all lives lost

--- www/sw.js
+++ www/sw.js
@@ -4,7 +4,7 @@
 
    Game update karne ke baad CACHE ka number barha dena, warna
    phone purani copy hi dikhata rahega. */
-var CACHE = "nerve-v31-left-column-powers";
+var CACHE = "nerve-v32-ad-revive";
 var FILES = [
   "./",
   "./index.html",
